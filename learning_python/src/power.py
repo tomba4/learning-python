1ed91a1db0f697cf98d0a9feba8f839f3982a412
tomas <tomas@tomba-pc>
2015-10-22 22:27:28 +0200

    Generate list with for

--- learning_python/src/power.py
+++ learning_python/src/power.py
@@ -3,10 +3,13 @@ Created on Oct 22, 2015
 
 @author: tomas
 '''
-L = [1, 2, 4, 8, 16, 32, 64]
+L = []
+for i in range(7): L.append(2 ** i)
+
+print(L)
 X = 5
 
 if 2 ** X in L:
-    print('at index', L.index(2 ** X))
+    print(2 ** X, 'at index', L.index(2 ** X))
 else:
     print(X, 'not found')
