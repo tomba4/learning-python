3e312739ce404048c2b5f6b33a424299bd188724
tomas <tomas@tomba-pc>
2015-10-22 22:16:54 +0200

    Eliminated the 'found' flag

--- learning_python/src/power.py
+++ learning_python/src/power.py
@@ -6,15 +6,12 @@ Created on Oct 22, 2015
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 
-found = False
 i = 0
-while not found and i < len(L):
-    if 2 ** X == L[i]:
-        found = True
-    else:
-        i = i + 1
 
-if found:
-    print('at index', i)
+while i < len(L):
+    if 2 ** X == L[i]:
+        print('at index', i)
+        break 
+    i += 1
 else:
     print(X, 'not found')
