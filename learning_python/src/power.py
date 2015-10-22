38a8f7fee6871607efdb7ef189c32323b63c3519
tomas <tomas@tomba-pc>
2015-10-22 22:07:53 +0200

    Initial version of power.py

---
+++ learning_python/src/power.py
@@ -0,0 +1,20 @@
+'''
+Created on Oct 22, 2015
+
+@author: tomas
+'''
+L = [1, 2, 4, 8, 16, 32, 64]
+X = 5
+
+found = False
+i = 0
+while not found and i < len(L):
+    if 2 ** X == L[i]:
+        found = True
+    else:
+        i = i + 1
+
+if found:
+    print('at index', i)
+else:
+    print(X, 'not found')
