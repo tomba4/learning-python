a8587795c59e6b751ab9bd2d99abcf56b758314f
tomas <tomas@tomba-pc>
2015-10-22 22:20:34 +0200

    for instead of while

--- learning_python/src/power.py
+++ learning_python/src/power.py
@@ -6,12 +6,9 @@ Created on Oct 22, 2015
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
 
-i = 0
-
-while i < len(L):
-    if 2 ** X == L[i]:
-        print('at index', i)
+for i in L:
+    if 2 ** X == i:
+        print('at index', L.index(i))
         break 
-    i += 1
 else:
     print(X, 'not found')
